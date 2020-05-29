e8876c80a7582a99108fb1b4d949485aefd491b5
rfrm1 <ruimoreira1997@gmail.com>
2020-05-29 18:23:56 +0100

    collider zona segura

---
+++ VR/Assets/Scripts/colliderZonaPermitida.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class colliderZonaPermitida : MonoBehaviour
+{
+    public GameObject zonapermitidaobj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        zonapermitidaobj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("ZonaPermitida"))
+        {
+            zonapermitidaobj.SetActive(true);
+        }
+        else
+        if(other.gameObject.tag.Equals("podeandar"))
+        {
+            zonapermitidaobj.SetActive(false);
+        }
+    }
+}
