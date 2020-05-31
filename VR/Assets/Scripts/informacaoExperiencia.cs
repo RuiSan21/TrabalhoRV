5b5dd1c58a1b9b0346be147f61fc60d3c02d88a4
rfrm1 <ruimoreira1997@gmail.com>
2020-05-31 16:55:50 +0100

    help menu

---
+++ VR/Assets/Scripts/informacaoExperiencia.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class informacaoExperiencia : MonoBehaviour
+{
+    public GameObject informacaoExp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        informacaoExp.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            informacaoExp.SetActive(false);
+        }
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            informacaoExp.SetActive(true);
+        }
+    }
+}
